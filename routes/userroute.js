91e3c8ac1b7449899f6d5b690eef493e798c4995
Manager <manageratbaseline@gmail.com>
2025-09-23 11:36:15 +0530

    fetcher sender and receiver

--- routes/userroute.js
+++ routes/userroute.js
@@ -13,4 +13,14 @@ router.get('/users',async(req,res)=>{
     }
 })
 
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("name profilePic");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 export default router;
