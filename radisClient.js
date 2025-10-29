5e318eeb4a778a564bf3905e9577c3b6f124ba36
Manager <manageratbaseline@gmail.com>
2025-10-29 10:25:35 +0530

    added video call

--- radisClient.js
+++ radisClient.js
@@ -1,27 +1,3 @@
-// import Redis from 'ioredis';
-// // The Redis connection string from your environment variables
-// const redisUrl = process.env.REDIS_URL; 
-
-// // The 'redis' client library is not needed if you're using 'ioredis'
-// // const client = createClient(); 
-
-// const redis = new Redis(redisUrl, {
-//   tls: {
-//     // This is often needed for Redis Cloud connections
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// redis.on('connect', () => {
-//   console.log('✅ Connected to Redis successfully!');
-// });
-
-// redis.on('error', (err) => {
-//   console.error('❌ Redis connection error:', err);
-// });
-
-// export default redis;
-
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -29,8 +5,8 @@ const client = createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-17845.c278.us-east-1-4.ec2.redns.redis-cloud.com',
-        port: 17845
+        host: 'redis-14508.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 14508
     }
 });
 
